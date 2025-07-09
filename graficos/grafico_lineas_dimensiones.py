fa7a65d465c0b2f31d93e300f2578836222c10db
RamiroPadilla <ramiro_mdq@live.com>
2025-07-09 15:05:45 -0300

    ticks eje x

--- graficos/grafico_lineas_dimensiones.py
+++ graficos/grafico_lineas_dimensiones.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors as mcolors
 
+#%%
 df = pd.read_csv('../resultados_dimensional.csv')
 
 df = df.sort_values('dimension')
@@ -31,6 +32,8 @@ df = pd.read_csv('../resultados_lineal.csv')
 
 df = df.sort_values('dimension')
 
+cmap = cm.get_cmap('viridis', 3)
+
 for i, algo in enumerate(["weiszfeld", "desc_coord", "desc_grad_armijo"]):
     datos_alg = df[df['algoritmo'] == algo]
     plt.plot(datos_alg['dimension'], datos_alg['tiempo_computo'], color=cmap(i),
@@ -38,10 +41,31 @@ for i, algo in enumerate(["weiszfeld", "desc_coord", "desc_grad_armijo"]):
 
 plt.xlabel('Dimensión')
 plt.ylabel('Tiempo de cómputo (s)')
+plt.xticks(np.arange(5, 51, 5))
 plt.legend(title='Algoritmo')
 plt.grid(True)
 plt.tight_layout()
 plt.savefig(fname = f'grafico_dimensiones_lineal',dpi=250, bbox_inches='tight', pad_inches=0.2)
 plt.close()
 #
+# %%
+df = pd.read_csv('../resultados_uniforme.csv')
+
+df = df.sort_values('dimension')
+
+cmap = cm.get_cmap('viridis', 3)
+
+for i, algo in enumerate(["weiszfeld", "desc_coord", "desc_grad_armijo"]):
+    datos_alg = df[df['algoritmo'] == algo]
+    plt.plot(datos_alg['dimension'], datos_alg['tiempo_computo'], color=cmap(i),
+             marker='o', label=algo)
+
+plt.xlabel('Dimensión')
+plt.ylabel('Tiempo de cómputo (s)')
+plt.xticks(np.arange(5, 51, 5))
+plt.legend(title='Algoritmo')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig(fname = f'grafico_dimensiones_uniforme',dpi=250, bbox_inches='tight', pad_inches=0.2)
+plt.close()
 # %%
